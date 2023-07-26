3595f01d097d87ee5068bd1a7c2080bd883224c1
David Bokser <dbokser@gmail.com>
2023-07-25 22:05:05 -0400

    Adding noise to key easer tool

--- cgm/core/tools/keyEaser.py
+++ cgm/core/tools/keyEaser.py
@@ -10,12 +10,8 @@ Example ui to start from
 ================================================================
 """
 # From Python =============================================================
-import copy
-import re
-import time
-import pprint
-import os
 import logging
+import math
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
@@ -34,7 +30,7 @@ import cgm.core.lib.transform_utils as TRANS
 from cgm.core.cgmPy import path_Utils as CGMPATH
 import cgm.core.lib.math_utils as MATH
 from cgm.lib import lists
-
+from cgm.core.lib import noise
 
 
 #>>> Root settings =============================================================
@@ -50,7 +46,7 @@ class ui(cgmUI.cgmGUI):
     MIN_BUTTON = True
     MAX_BUTTON = False
     FORCE_DEFAULT_SIZE = True  #always resets the size of the window when its re-created  
-    DEFAULT_SIZE = 425,170
+    DEFAULT_SIZE = 425,190
     TOOLNAME = '{0}.ui'.format(__toolname__)
     UNDO_CHUNK_NAME = "undo-keyser-drag"
 
@@ -66,6 +62,7 @@ class ui(cgmUI.cgmGUI):
         self.__toolName__ = self.__class__.WINDOW_NAME	
 
         self._isChunkOpen = False
+        self._selected_objects = []
 
         #self.l_allowedDockAreas = []
         self.WINDOW_TITLE = self.__class__.WINDOW_TITLE
@@ -91,7 +88,7 @@ class ui(cgmUI.cgmGUI):
         _str_func = 'build_layoutWrapper'
 
         _MainForm = mUI.MelFormLayout(self,ut='cgmUITemplate')
-        _column = self.buildColumn_main(_MainForm,True)
+        _column = self.buildColumn_main(_MainForm)
 
     
         _row_cgm = cgmUI.add_cgmFooter(_MainForm)            
@@ -107,9 +104,35 @@ class ui(cgmUI.cgmGUI):
                   ac = [(_column,"bottom",2,_row_cgm),
                         ],
                   attachNone = [(_row_cgm,"top")])          
-        
+    
+    def buildColumn_main(self, parent):
+        mc.setParent(parent)
+        self.uiTabLayout = mc.tabLayout()
+        print("My tab layout -- %s"%self.uiTabLayout)
 
-    def buildColumn_main(self,parent, asScroll = False):
+        self.easeColumn = self.buildColumn_ease(self.uiTabLayout, asScroll=True)
+        self.noiseColumn = self.buildColumn_noise(self.uiTabLayout, asScroll=True)
+
+        mc.tabLayout(self.uiTabLayout,
+            edit=True,
+            tabLabel=(
+                (self.easeColumn, "Ease"),
+                (self.noiseColumn, "Noise"),
+            ),
+            cc=lambda *a: mc.evalDeferred(
+                cgmGEN.Callback(self.uiFunc_handleTabChange), lp=True
+            ),
+        )
+
+        return self.uiTabLayout
+    
+    def uiFunc_handleTabChange(self):
+        print("Tab changed")
+
+    #############################################
+    # Ease
+    #############################################
+    def buildColumn_ease(self,parent, asScroll = False):
         if asScroll:
             _inside = mUI.MelScrollLayout(parent,useTemplate = 'cgmUISubTemplate') 
         else:
@@ -181,30 +204,121 @@ class ui(cgmUI.cgmGUI):
 
         return _inside
 
+    #############################################
+    # Noise
+    #############################################
+    def buildColumn_noise(self,parent, asScroll = False):
+        if asScroll:
+            _inside = mUI.MelScrollLayout(parent,useTemplate = 'cgmUISubTemplate') 
+        else:
+            _inside = mUI.MelColumnLayout(parent,useTemplate = 'cgmUISubTemplate') 
+
+        defaultAmplitude = 5
+        defaultFrequency = .5
+
+        _row = mUI.MelHSingleStretchLayout(_inside, expand=True, ut="cgmUISubTemplate")
+        mUI.MelSpacer(_row, w=5)
+        mUI.MelLabel(_row, l="Amplitude:", align="right")
+        self.uiTF_noise_amplitude = mUI.MelFloatField(
+            _row,
+            minValue=0.0,
+            value=defaultAmplitude,
+            precision=2,
+            annotation="Amplitude"
+        )
+        self.uiTF_noise_amplitude(edit=True, changeCommand=cgmGEN.Callback(self.uiFunc_setAmplitudeSlider, "field"))
+        
+        self.uiSlider_noise_amplitude = mUI.MelFloatSlider(_row,0.0, 10.0, defaultAmplitude, step=0.01)
+        self.uiSlider_noise_amplitude.setValue(defaultAmplitude)
+        cmd = cgmGEN.Callback(self.uiFunc_setAmplitudeSlider, "slider")
+
+        self.uiSlider_noise_amplitude(edit=True, changeCommand=cgmGEN.Callback(self.uiFunc_handleChange, cmd))
+        self.uiSlider_noise_amplitude(edit=True, dragCommand=cgmGEN.Callback(self.uiFunc_handleDrag, cmd))
+
+        _row.setStretchWidget(self.uiSlider_noise_amplitude)
+        mUI.MelSpacer(_row, w=5)
+        _row.layout()
+
+        _row = mUI.MelHSingleStretchLayout(_inside, expand=True, ut="cgmUISubTemplate")
+        mUI.MelSpacer(_row, w=5)
+        mUI.MelLabel(_row, l="Frequency:", align="right")
+        self.uiTF_noise_frequency = mUI.MelFloatField(
+            _row,
+            minValue=0.0,
+            value=defaultFrequency,
+            precision=2,
+            annotation="Frequency"
+        )
+        
+        self.uiTF_noise_frequency(edit=True, changeCommand=cgmGEN.Callback(self.uiFunc_setFrequencySlider, "field"))
+        
+        self.uiSlider_noise_frequency = mUI.MelFloatSlider(_row,0.0, 1.0, defaultFrequency, step=0.01)
+        self.uiSlider_noise_frequency.setValue(defaultFrequency)
+        cmd = cgmGEN.Callback(self.uiFunc_setFrequencySlider, "slider")
+        
+        self.uiSlider_noise_frequency(edit=True, changeCommand=cgmGEN.Callback(self.uiFunc_handleChange, cmd))
+        self.uiSlider_noise_frequency(edit=True, dragCommand=cgmGEN.Callback(self.uiFunc_handleDrag, cmd))
+
+        _row.setStretchWidget(self.uiSlider_noise_frequency)
+        mUI.MelSpacer(_row, w=5)
+        _row.layout()
+        
+        # Generate Button
+        #
+        _row = mUI.MelHLayout(_inside, ut="cgmUISubTemplate", padding=10)
+
+        self.generateBtn = cgmUI.add_Button(
+            _row,
+            "Bring the Noise",
+            cgmGEN.Callback(self.uiFunc_setAmplitudeSlider, "field"),
+            "Set",
+            h=50,
+        )
+
+        _row.layout()
+
+        return _inside
+
     def uiFunc_handleDrag(self, func):
         if not self._isChunkOpen:
             mc.undoInfo(openChunk=True,chunkName=self.UNDO_CHUNK_NAME)
             self._isChunkOpen = True
-        
+            self._selected_objects = get_selected_keys()
+            cmd = cgmGEN.Callback(self.uiFunc_closeChunk)
+            self.scriptJobs = []
+            self.scriptJobs.append(mc.scriptJob( e= ["Undo",cmd], protected=True, ro=True))
+            self.scriptJobs.append(mc.scriptJob( e= ["SelectionChanged",cmd], protected=True, ro=True))
+
         func()
 
-    def uiFunc_handleChange(self, func):
+    def uiFunc_handleChange(self, func):        
+        func()
+
+    def uiFunc_closeChunk(self):
         if self._isChunkOpen:
             mc.undoInfo(closeChunk=True)
             self._isChunkOpen = False
-        
-        func()
-
+            self._selected_objects = []
+            
     def uiFunc_setFavorSlider(self, source):
         uiFunc_setFieldSlider(self.uiTF_favor, self.uiSlider_favor, source, 1.0, .01)
 
-        adjust_favoring(self.uiSlider_favor.getValue(), self.uiSlider_midPoint.getValue())
+        adjust_favoring(self._selected_objects, self.uiSlider_favor.getValue(), self.uiSlider_midPoint.getValue())
 
     def uiFunc_setMidPointSlider(self, source):
         uiFunc_setFieldSlider(self.uiTF_midPoint, self.uiSlider_midPoint, source, 1.0, .01)
 
-        adjust_favoring(self.uiSlider_favor.getValue(), self.uiSlider_midPoint.getValue())
+        adjust_favoring(self._selected_objects, self.uiSlider_favor.getValue(), self.uiSlider_midPoint.getValue())
 
+    def uiFunc_setAmplitudeSlider(self, source):
+        uiFunc_setFieldSlider(self.uiTF_noise_amplitude, self.uiSlider_noise_amplitude, source, 10.0, .01)
+
+        adjust_noise(self._selected_objects, self.uiSlider_noise_amplitude.getValue(), self.uiSlider_noise_frequency.getValue())
+
+    def uiFunc_setFrequencySlider(self, source):
+        uiFunc_setFieldSlider(self.uiTF_noise_frequency, self.uiSlider_noise_frequency, source, 1.0, .01)
+
+        adjust_noise(self._selected_objects, self.uiSlider_noise_amplitude.getValue(), self.uiSlider_noise_frequency.getValue())
 
 def uiFunc_setFieldSlider(field, slider, source, maxVal=100, step=1):
     _value = 0
@@ -219,8 +333,6 @@ def uiFunc_setFieldSlider(field, slider, source, maxVal=100, step=1):
 
     return _value
     
-
-
 def lerp(a, b, t):
     """
     Performs a linear interpolation between two values a and b using ratio t
@@ -231,7 +343,6 @@ def lerp(a, b, t):
     :return: The interpolated value
     """
     return (1 - t) * a + t * b
-
 
 def cubic_bezier(t, p0, p1, p2, p3, ease_factor):
     """
@@ -252,38 +363,6 @@ def cubic_bezier(t, p0, p1, p2, p3, ease_factor):
     result += 3 * u * tt * p2 # Bezier control point 2
     result += tt * p3 # for the end
     return result
-
-
-# def get_selected_keys():
-#     selected_keys = []
-#     # Get the list of all selected objects
-#     objects = mc.ls(selection=True)
-    
-#     for obj in objects:
-#         # List all keyable attributes of the object
-#         keyable_attrs = mc.listAttr(obj, keyable=True)
-        
-#         for attr in keyable_attrs:
-#             # Get full name of the attribute including the object name
-#             attr_full_name = "{}.{}".format(obj, attr)
-            
-#             # Check if the attribute has any keys
-#             if mc.keyframe(attr_full_name, query=True, selected=True):
-#                 # List all the keys of the attribute
-#                 key_times = mc.keyframe(attr_full_name, query=True, timeChange=True, selected=True)
-#                 key_values = mc.keyframe(attr_full_name, query=True, valueChange=True, selected=True)
-#                 keys = zip(key_times, key_values)
-                
-#                 # Add each key to the list
-#                 for key in keys:
-#                     selected_keys.append({
-#                         "object": obj,
-#                         "attribute": attr,
-#                         "time": key[0],
-#                         "value": key[1],
-#                     })
-    
-#     return selected_keys
 
 def get_selected_keys():
     selected_keys = []
@@ -330,66 +409,9 @@ def easeIn(t, power):
 
 def easeOut(t, power):
     return 1 - (1 - t)**power
-    
-# def adjust_favoring(favor = 1.0, midpoint=0.5):
-#     favor = favor * 10.0 + 1
-    
-#     # Get the currently selected keys
-#     selected_keys = get_selected_keys()
 
-#     if len(selected_keys) < 3:
-#         print("Please select at least three keys to adjust favoring.")
-#         return
-
-#     # Get the first and last key
-#     first_key = selected_keys[0]
-#     last_key = selected_keys[-1]
-
-#     # Calculate the midpoint in value and time
-#     midValue = lerp(first_key["value"], last_key["value"], midpoint)
-#     midTime = lerp(first_key["time"], last_key["time"], midpoint)
-    
-#     #key = selected_keys[3]
-#     #favor = 2.0
-#     # Iterate over all keys
-#     for key in selected_keys[1:-1]:
-#         keyFavor = 1.0 - favor
-#         func = easeInOut
-#         if key["time"] < midTime:
-#             first_key = selected_keys[0]
-#             last_key = copy.copy(selected_keys[-1])
-#             last_key["time"] = midTime
-#             last_key["value"] = midValue
-#             func = easeIn
-#         else:
-#             first_key = copy.copy(selected_keys[0])
-#             first_key["time"] = midTime
-#             first_key["value"] = midValue
-#             last_key = selected_keys[-1]
-#             func = easeOut
-
-#         # Calculate the ratio
-#         ratio = (key["time"] - first_key["time"]) / (last_key["time"] - first_key["time"])
-
-#         # Adjust the ratio with the easeInOutQuint function
-#         ratio = func(ratio, favor)
-
-#         # Calculate the new value
-#         new_value = lerp(first_key["value"],last_key["value"], ratio)
-
-#         # Set the new key value
-#         attr_full_name = "{}.{}".format(key["object"], key["attribute"])
-#         mc.setKeyframe(attr_full_name, time=key["time"], value=new_value)
-#         mc.keyTangent(attr_full_name, time=(key["time"],), edit=True, inTangentType='auto', outTangentType='auto')
-    
-#     mc.keyTangent(attr_full_name, time=(selected_keys[0]["time"],), edit=True, inTangentType='auto', outTangentType='auto')
-#     mc.keyTangent(attr_full_name, time=(selected_keys[-1]["time"],), edit=True, inTangentType='auto', outTangentType='auto')
-
-def adjust_favoring(favor=1.0, midpoint=0.5):
+def adjust_favoring(selected_objects, favor=1.0, midpoint=0.5):
     favor = favor * 10.0 + 1
-
-    # Get the currently selected keys
-    selected_objects = get_selected_keys()
 
     # Iterate over all objects and their attributes
     for object_dict in selected_objects:
@@ -438,30 +460,20 @@ def adjust_favoring(favor=1.0, midpoint=0.5):
                 mc.keyTangent(attr_full_name, time=(keys[-1][0],), edit=True, inTangentType='auto', outTangentType='auto')
             mc.dgdirty(obj)
 
-def set_values_by_ratio(ratio = 0):
-    # Get the currently selected keys
-    selected_objects = get_selected_keys()
-
-    # Iterate over all objects and their attributes
+def adjust_noise(selected_objects, amplitude=0, frequency=0.5):
     for object_dict in selected_objects:
+        # add noise to each key value based on time multiplied by frequency and amplitude
         for obj, attr_dict in object_dict.items():
             for attr, keys in attr_dict.items():
                 if len(keys) < 2: # We need at least two keys to interpolate between
                     print("For object {} and attribute {}, please select at least two keys.".format(obj,attr))                    
                     continue
 
-                # Get the first and last key values
-                first_key_value = keys[0][1]
-                last_key_value = keys[-1][1]
-
-                # Get total time range
-                total_time = keys[-1][0] - keys[0][0]
-
                 # Iterate over all keys
                 for key in keys:
-                    # Calculate the new value using lerp
-                    new_value = lerp(first_key_value, last_key_value, ratio)
-
+                    new_value = key[1] + noise.noise(key[0] * frequency + keys[int(len(keys)/2)][0]/30.0, keys[0][1]) * amplitude
                     # Set the new key value
                     attr_full_name = "{}.{}".format(obj, attr)
                     mc.setKeyframe(attr_full_name, time=key[0], value=new_value)
+
+                mc.dgdirty(obj)
